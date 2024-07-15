80fbf2065327311e9c11d0b684f88913c2a2f7f5
Satyam singh <satyamsingh2003a@gmail.com>
2024-07-15 17:28:20 +0530

    implement vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css';
+import { Analytics } from "@vercel/analytics/react";
+import "../styles/globals.css"; // Import your global CSS file if you have one
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
